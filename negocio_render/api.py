384fe29e37ba6c3d41e5e231508b1f7cd1cce981
Overx18 <jricastica90@gmail.com>
2025-02-02 19:44:45 -0500

    Updated

--- negocio_render/api.py
+++ negocio_render/api.py
@@ -1,4 +1,3 @@
-# negocio/api.py
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
@@ -102,5 +101,5 @@ def server_error(error):
     return jsonify({"error": "Error interno del servidor"}), 500
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
